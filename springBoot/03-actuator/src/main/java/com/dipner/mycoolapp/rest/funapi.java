dee6159d666559c8113f53c234864f4dc05b3af5
A Dipner Reddy <89031602+dipnerreddy@users.noreply.github.com>
2023-09-07 12:23:54 +0530

    Add files via upload

---
+++ springBoot/03-actuator/src/main/java/com/dipner/mycoolapp/rest/funapi.java
@@ -0,0 +1,18 @@
+package com.dipner.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class funapi {
+    @GetMapping("/greeting")
+    public String Greeting(){
+        return "Hello, It is a beautiful day out there.\n Isn't it?😄";
+    }
+
+    @GetMapping("/task")
+    public String task(){
+        return "the take is done only once";
+    }
+
+}
